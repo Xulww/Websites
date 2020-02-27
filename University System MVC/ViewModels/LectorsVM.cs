9321b6970904416c6f7e310199d68a49d0bb4674
Jordan <49492517+Xulww@users.noreply.github.com>
2020-02-27 18:57:45 +0200

    Add files via upload

---
+++ University System MVC/ViewModels/LectorsVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace University.ViewModels
+{
+    public class LectorsVM
+    {
+        public int LectorID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        //storing all the partners that lectors can be associated with
+        public List<CheckBoxVM> Partners { get; set; }
+    }
+}
