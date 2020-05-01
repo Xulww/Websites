90184129ef921636ec2a874386abd8b68de1cf9f
Jordan <49492517+Xulww@users.noreply.github.com>
2020-05-01 15:43:25 +0300

    Add files via upload

---
+++ *REACT* Groshaway Groceries/src/containers/Billa/Billa.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Billa = () => {
+    return (
+        <div>
+            <h1>Billa</h1>
+            <h3>Coming soon...</h3>
+        </div>
+    );
+}
+
+export default Billa;
